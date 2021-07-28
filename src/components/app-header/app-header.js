40ad6c99a6b08791cbc302033e06dcd595189760
divelineq <divelineq@gmail.com>
2021-07-29 00:04:06 +0700

    full

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './app-header.css'
+
+
+
+const AppHeader = ({liked, allPosts}) => {
+  return(
+    <div className="app-header d-flex">
+      <h1>DMITRY MOROZOV</h1>
+      <h2>{allPosts} запсей, из них понравилось {liked}</h2>
+    </div>
+  )
+}
+
+export default AppHeader;
